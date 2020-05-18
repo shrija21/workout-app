9bb49d0902c1347374a3adbff4ca767e3cea4910
shri <shrija.tumma1994@gmail.com>
2020-05-18 12:49:57 +0530

    Added route, index and new actions of exercises controller

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,10 @@
+class ExercisesController < ApplicationController
+
+	def index
+		
+	end
+
+	def new
+		@exercise = current_user.exercise.new
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	root 'dashboards#index'  
+	root 'dashboards#index' 
+
+	resources :users do
+		resources :exercises
+	end
 end
