debed5362adb128f3a5dff273c60060d51de226e
shri <shrija.tumma1994@gmail.com>
2020-05-18 16:51:33 +0530

    Write creating exercise with invalid inputs feature

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+
+  validates :duration_in_min, presence: true, numericality: { only_integer: true }
+  validates :workout, presence: { message: "Workout details can't be blank" }
+  validates :workout_date, presence: { message: "Activity date can't be blank" }
 end
