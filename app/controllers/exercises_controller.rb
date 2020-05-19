118a2f366c4dc03d60748c571c95a68141d30480
shri <shrija.tumma1994@gmail.com>
2020-05-19 20:19:42 +0530

    Write editing exercise feature spec

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -7,7 +7,7 @@ class ExercisesController < ApplicationController
 
 	def show
 		# byebug
-		@exercise = current_user.exercises.find_by params[:id]
+		@exercise = current_user.exercises.find params[:id]
 	end
 
 	def new
@@ -24,6 +24,22 @@ class ExercisesController < ApplicationController
 		else
 			flash.now[:alert] = "Exercise has not been created"
 			render :new
+		end
+	end
+
+	def edit
+		@exercise = current_user.exercises.find params[:id]
+	end
+
+	def update
+		@exercise = current_user.exercises.find params[:id]
+
+		if @exercise.update(exercise_params)
+			flash[:notice] = "Exercise has been updated"
+			redirect_to [current_user, @exercise]	
+		else
+			flash.now[:alert] = "Exercise has not been updated"
+			render :edit
 		end
 	end
 
