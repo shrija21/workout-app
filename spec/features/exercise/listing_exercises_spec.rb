21c940b4a40676ba9283c912a53694df1f4d0ef8
shri <shrija.tumma1994@gmail.com>
2020-05-20 23:14:20 +0530

    Display friends on workout index page

--- spec/features/exercise/listing_exercises_spec.rb
+++ spec/features/exercise/listing_exercises_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "Listing Exercises" do
 	before do
 		@john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+		@sarah = User.create(first_name: "Sarah", last_name: "Anderson", email: "sarah@example.com", password: "password")
 		login_as(@john)
 
 		@e1 = @john.exercises.create(duration_in_min: 20,
@@ -16,6 +17,7 @@ RSpec.feature "Listing Exercises" do
 		@e3 = @john.exercises.create(duration_in_min: 35,
 																	workout: "On treadmil",
 																	workout_date: 8.days.ago)
+		@following = Friendship.create(user: @john, friend: @sarah)
 	end
 
 	scenario "shows user's workout for last 7 days" do
@@ -43,5 +45,14 @@ RSpec.feature "Listing Exercises" do
 		click_link "My Lounge"
 
 		expect(page).to have_content("No Workouts Yet")
+	end
+
+	scenario "show a list of user's friends" do
+		visit "/"
+
+		click_link "My Lounge"
+		expect(page).to have_content("My Friends")
+		expect(page).to have_link(@sarah.full_name)
+		expect(page).to have_link("Unfollow")
 	end
 end
